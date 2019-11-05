838f55d14a6e4f583a3db72063aedbdbc4b9e474
aminebech <amine.bechouaia@gmail.com>
2019-11-05 17:22:36 +0100

    commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import '../components/style.css';
 
-const Contact = (props) => {
+
+class Contact extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        online: false,
+      };
+    }
+    render () {
     return (
         <div className="Contact">
-            <img className="avatar" src={props.avatar} alt=""/>
+            <img className="avatar" src={this.props.avatar} alt=""/>
             <div>
-                <div className="name">{props.name}</div>
+                <div className="name">{this.props.name}</div>
                 <div className="status">
                     <div className="status-text">
-                        <div className={props.online === "true" ? 'status-online' : 'status-offline'}></div>
-                            {props.online === "true" ? "Online" : "Offline"}
+                        <div  className={this.state.online ? 'online' : 'offline'}
+                onClick={event => {
+                const Offline = !this.state.online;
+                this.setState({ online: Offline });
+              }}></div>
+                            {this.props.online === "true" ? "Online" : "Offline"}
                     </div>
                 </div>
             </div>
          </div>
     )
+}
 }
 
 export default Contact;
